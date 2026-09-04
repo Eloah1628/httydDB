13f959109e9554a651c182be6f01d3393298a8bd
Eloah <eloahgabrielesantos@gmail.com>
2026-09-04 13:57:42 -0300

    Primeiro commit de teste

---
+++ index.js
@@ -0,0 +1,234 @@
+
+/*
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+    res.send("olá mundo")
+})
+
+
+res.status(200).json({msg : "Listando..."})
+app.listen(3000, () => {
+    console.log("Site funcionando em http://localhost:3000");
+})
+*/
+const express = require("express");
+const { Pool } = require("pg");
+const app = express();
+app.use(express.json());
+
+const db = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "seu_banco",
+    password: "sua_senha",
+    port: 5432
+});
+
+//Cavaleiro
+app.get("/Cavaleiro", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.get("/Cavaleiro/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando por ID..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.post("/Cavaleiro", async (req, res, next) => {
+    const Genero = ["F", "f", "M", "m"];
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        
+    }
+    
+})
+
+app.put("/Cavaleiro/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Algo deu errado na edição."})        
+    }
+})
+
+app.delete("/Cavaleiro/:id", (req, res, next) => {
+    try {
+        if (condition) {
+
+
+            res.status(200).json({msg : "Deletado com sucesso!"})  
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(400).json({msg : "Não encontrado!"})        
+    }
+})
+
+
+//Dragão
+app.get("/Dragao", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.get("/Dragao/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando por ID..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.post("/Dragao", (req, res, next) => {
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(400).json({msg :"Não foi possível cadastrar."})
+    }
+})
+
+app.put("/Dragao/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Algo deu errado na edição."})        
+    }
+})
+
+app.delete("/Dragao/:id", (req, res, next) => {
+    try {
+        if (condition) {
+
+            
+            res.status(200).json({msg : "Deletado com sucesso!"})  
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(400).json({msg : "Não encontrado!"})        
+    }
+})
+
+//Treinamento
+app.get("/Treinamento", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.get("/Treinamento/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            
+            
+            res.status(200).json({msg : "Listando por ID..."})
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Não encontrado!"});        
+    }
+})
+
+app.post("/Treinamento", (req, res, next) => {
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(400).json({msg :"Não foi possível cadastrar."})
+    }
+})
+
+app.put("/Treinamento:id", () => {
+    try {
+        if (condition) {
+            
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(404).json({msg : "Algo deu errado na edição."})        
+    }
+})
+
+app.delete("/Treinamento/:id", (req, res, next) => {
+    try {
+        if (condition) {
+            res.status(200).json({msg : "Deletado com sucesso!"})  
+        } else {
+            
+        }
+    } catch (error) {
+        res.status(400).json({msg : "Não encontrado!"})        
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Site funcionando em http://localhost:3000");
+});
---
+++ init.sql
@@ -0,0 +1,49 @@
+/*===========================================
+
+Relacionamento baseado em HTTYD;
+            Tabelas:
+1. Cavaleiro
+2. Dragao
+3. Treinamento
+*/
+
+CREATE TABLE IF NOT EXISTS Cavaleiro(
+    ID SERIAL PRIMARY KEY,
+    Nome VARCHAR(20) NOT NULL UNIQUE,
+    Sobrenome VARCHAR(20) NOT NULL,
+    Idade INTEGER NOT NULL CHECK (Idade BETWEEN 14 AND 80),
+    Sexo CHAR(1) NOT NULL CHECK (Sexo IN ('F', 'f', 'M', 'm')),
+    Funcao VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Dragao(
+    ID SERIAL PRIMARY KEY,
+    Nome VARCHAR(20) NOT NULL UNIQUE,
+    Especie VARCHAR(40) NOT NULL,
+    Sexo CHAR(1) NOT NULL CHECK (Sexo IN ('F', 'f', 'M', 'm')),
+    Tipo VARCHAR(20) NOT NULL CHECK (Tipo IN ('Boulder', 'Mystery', 'Sharp', 'Stoker', 'Strike', 'Tidal', 'Tracker')),
+    TreinamentoID INTEGER NOT NULL,
+    CavaleiroID INTEGER NOT NULL,
+
+    /*FK*/
+    CONSTRAINT FK_DragaoCavaleiro FOREIGN KEY (CavaleiroID) REFERENCES Cavaleiro(ID)
+);
+
+CREATE TABLE IF NOT EXISTS Treinamento(
+    ID SERIAL PRIMARY KEY,
+    Tipo VARCHAR(20) NOT NULL CHECK,
+    QuemRealiza INTEGER NOT NULL,
+    DragaoParticipante INTEGER NOT NULL,
+    Tempo TIME NOT NULL,
+
+    /*FK*/
+    CONSTRAINT FK_TreinamentoCavaleiro FOREIGN KEY(QuemRealiza) REFERENCES Cavaleiro(ID),
+    CONSTRAINT FK_TreinamentoDragao FOREIGN KEY(DragaoParticipante) REFERENCES Dragao(ID)
+);
+
+/*FK*/
+ALTER TABLE Cavaleiro ADD COLUMN DragaoID INTEGER NOT NULL;
+ALTER TABLE Cavaleiro ADD CONSTRAINT FK_CavaleiroPossuiDragao FOREIGN KEY(DragaoID) REFERENCES Dragao(ID);
+
+/*JOIN's*/
+SELECT Dragao.CavaleiroID, Cavaleiro.ID FROM Dragao JOIN Cavaleiro ON Dragao.CavaleiroID = Cavaleiro.ID 
