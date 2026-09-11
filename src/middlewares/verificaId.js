08ea68a02e781fe9ba8e54f0430af4a4345a64f5
Eloah <eloahgabrielesantos@gmail.com>
2026-09-11 15:56:04 -0300

    adiciona mensagens de retorno

--- src/middlewares/verificaId.js
+++ src/middlewares/verificaId.js
@@ -4,16 +4,16 @@ function VerificaId(tabela) {
 
         if (!/^\d+$/.test(id) || Number(id) < 1) {
             return res.status(400).json({msg : "ID inválido"});
-        }
-        try {
+        } else {
+            try {
             const resultado = await db.query(`SELECT * FROM ${tabela} WHERE ID = $1`, [id]);
             if (resultado.rows.length === 0) {
                 return res.status(404).json({msg : "ID não encontrado"});
             }
-        } catch (erro) {
-            return res.status(500).json({msg : "Erro na verificação do ID"});
+            } catch (erro) {
+                return res.status(500).json({msg : "Erro na verificação do ID"});
+            } next();
         }
-        next();
     }
 }
 
