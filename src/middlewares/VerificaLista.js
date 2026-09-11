88265a464e721378b023b16f3864e36d17edaff8
Eloah <eloahgabrielesantos@gmail.com>
2026-09-11 12:29:55 -0300

    atualiza a middleware verificaid para conferir se ele não é uma letra

--- src/middlewares/VerificaLista.js
+++ src/middlewares/VerificaLista.js
@@ -1,5 +1,10 @@
 function VerificaLista(lista, valor) {
-    
+    return function (req, res, next) {
+        const item = req.params[valor];
+        if (!lista.includes(item)) {
+            return res.status(400).json({msg : `${valor} inválido`});
+        }
+    }
 }
 
 module.exports = VerificaLista;
--- src/middlewares/verificaId.js
+++ src/middlewares/verificaId.js
@@ -1,18 +1,17 @@
 function VerificaId(tabela) {
     return async function (req, res, next) {
         const id = req.params.id;
-        if (id < 1) {
-            return res.status(400).json({msg: "ID inválido"});
+
+        if (!/^\d+$/.test(id) || id < 1) {
+            return res.status(400).json({msg : "ID inválido"});
         }
         try {
-            const [resultado] = await db.query(`SELECT * FROM ${tabela} WHERE ID = ?`,
-    [id]);
+            const [resultado] = await db.query(`SELECT * FROM ${tabela} WHERE ID = $1`, [id]);
             if (resultado.length === 0) {
-                return res.status(404).json({msg: "ID não encontrado"});
+                return res.status(404).json({msg : "ID não encontrado"});
             }
-            next();
         } catch (erro) {
-            return res.status(500).json({msg: "Erro na verificação do ID"});
+            return res.status(500).json({msg : "Erro na verificação do ID"});
         }
     };
 }
