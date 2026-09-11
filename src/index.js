0e8995a0f97769525d27d73e35f89ce93d371c56
northonh <nobile.pedro@gmail.com>
2026-09-11 10:06:10 -0300

    Faz as funções basicas de /Treinamento

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.get("/Cavaleiro", async (req, res) => {
     } catch (error) {
         res.status(404).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 app.get("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     id = req.params.id;
@@ -36,7 +36,7 @@ app.get("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     } catch (error) {
         res.status(404).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 app.post("/Cavaleiro", VerificaLista(GenerosAceitaveis, "sexo"), async (req, res) => {
     const {nome, sobrenome, idade, sexo, funcao} = req.body;
@@ -49,9 +49,9 @@ app.post("/Cavaleiro", VerificaLista(GenerosAceitaveis, "sexo"), async (req, res
             res.status(200).json({msg : "Cavaleiro adicionado com sucesso!"});
         }
     } catch (error) {
-        res.status(400).json({msg :"Não foi possível cadastrar."})
+        res.status(400).json({msg :"Não foi possível cadastrar."});
     }
-})
+});
 
 app.put("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     const {nome, sobrenome, idade, sexo, funcao} = req.body;
@@ -59,9 +59,9 @@ app.put("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     try {
         const cavaleiro = await db.query("UPDATE Cavaleiro SET name=$1, sobrenome=$2, idade=$3, sexo=$4, funcao=$5 WHERE id=$6", [nome, sobrenome, idade, sexo, funcao, id]);
     } catch (error) {
-        res.status(404).json({msg : "Algo deu errado na edição."})        
+        res.status(404).json({msg : "Algo deu errado na edição."});        
     }
-})
+});
 
 app.delete("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     id = req.params.id;
@@ -72,109 +72,110 @@ app.delete("/Cavaleiro/:id", VerificaId("Cavaleiro"), async (req, res) => {
     } catch (error) {
         res.status(400).json({msg : "Não encontrado!"});       
     }
-})
+});
 
 
 //Dragão
 app.get("/Dragao", async (req, res) => {
     try {
-        const ListaDragoes = await db.query("SELECT * FROM Dragao")
-        res.status(200).json(ListaDragoes);
+        const ListaDragao = await db.query("SELECT * FROM Dragao");
+        res.status(200).json(ListaDragao);
     } catch (error) {
         res.status(404).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 app.get("/Dragao/:id", VerificaId("Dragao"), async (req, res) => {
     id = req.params.id;
     try {
-        res.status(200).json({msg : "Listando dragão por ID..."});
+        const dragao = await db.query("SELECT * FROM Dragao WHERE id = $1", [id]);
+        res.status(200).json(dragao);
     } catch (error) {
         res.status(404).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 app.post("/Dragao", VerificaLista(GenerosAceitaveis, "sexo"), VerificaLista(TiposAceitaveis, "tipo"), async (req, res) => {
     const {nome, especie, sexo, tipo, treinamentoID, cavaleiroID} = req.body;
     try {
-        await db.query("INSERT INTO Dragoes(nome, especie, sexo, tipo, treinamentoID, cavaleiroID) VALUES($1, $2, $3, $4, $5, $6)", [nome, especie, sexo, tipo, treinamentoID, cavaleiroID])
-        res.status(200).json({msg : "Dragão adicionado com sucesso!"})
+        await db.query("INSERT INTO Dragao(nome, especie, sexo, tipo, treinamentoID, cavaleiroID) VALUES($1, $2, $3, $4, $5, $6)", [nome, especie, sexo, tipo, treinamentoID, cavaleiroID]);
+        res.status(200).json({msg : "Dragão adicionado com sucesso!"});
     } catch (error) {
-        res.status(400).json({msg :"Não foi possível cadastrar."})
+        res.status(400).json({msg :"Não foi possível cadastrar."});
     }
-})
+});
 
 app.put("/Dragao/:id", VerificaId("Dragao"), VerificaLista(GenerosAceitaveis, "sexo"), VerificaLista(TiposAceitaveis, "tipo"), async (req, res) => {
     const {nome, especie, sexo, tipo, treinamentoID, cavaleiroID} = req.body;
     id = req.params.id;
     try {
-        await db.query("UPDATE Dragoes SET nome=$1, especie=$2, sexo=$3, tipo=$4, treinamentoID=$5, cavaleiroID=$6 WHERE id=$7", [nome, especie, sexo, tipo, treinamentoID, cavaleiroID, id]);
-        res.status(200).json({msg : "Dragão editado com sucesso!"})
+        await db.query("UPDATE Dragao SET nome=$1, especie=$2, sexo=$3, tipo=$4, treinamentoID=$5, cavaleiroID=$6 WHERE id=$7", [nome, especie, sexo, tipo, treinamentoID, cavaleiroID, id]);
+        res.status(200).json({msg : "Dragão editado com sucesso!"});
     } catch (error) {
-        res.status(404).json({msg : "Algo deu errado na edição."})        
+        res.status(404).json({msg : "Algo deu errado na edição."});        
     }
-})
+});
 
 app.delete("/Dragao/:id", VerificaId("Dragao"), async (req, res) => {
     id = req.params.id;
     try {
-        await db.query("DELETE FROM Dragoes WHERE id=$1", [id]);
+        await db.query("DELETE FROM Dragao WHERE id=$1", [id]);
         res.status(200).json({msg : "Treinamento adicionado com sucesso!"});
     } catch (error) {
         res.status(400).json({msg : "Não encontrado!"});  
     }
-})
+});
 
 //Treinamento
 app.get("/Treinamento", async (req, res) => {
     try {
-        
-        res.status(200).json({msg : "Treinamento adicionado com sucesso!"})
+        const ListaTreinamento = await db.query("SELECT * FROM Treinamento");
+        res.status(200).json(ListaTreinamento);
     } catch (error) {
         res.status(404).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 app.get("/Treinamento/:id", VerificaId("Treinamento"), async (req, res) => {
     id = req.params.id;
     try {
-        
-        res.status(200).json({msg : "Treinamento adicionado com sucesso!"})
+        const treinamento =  await db.query("SELECT * FROM Treinamento WHERE id = $1", [id]);      
+        res.status(200).json(treinamento);
     } catch (error) {
         res.status(404).json({msg : "Treinamento não encontrado!"});        
     }
-})
+});
 
 app.post("/Treinamento", async (req, res) => {
-    const {tipo, quemrealiza, dragaoparticipante, tempo} = req.body;
+    const {tipo, quemrealiza, Dragaoparticipante, tempo} = req.body;
     try {
-
-        res.status(200).json({msg : "Treinamento adicionado com sucesso!"})
+        await db.query("INSERT INTO Treinamento(tipo, quemrealiza, Dragaoparticipante, tempo) VALUES($1, $2, $3, $4)", [tipo, quemrealiza, Dragaoparticipante, tempo])
+        res.status(200).json({msg : "Treinamento adicionado com sucesso!"});
     } catch (error) {
-        res.status(400).json({msg :"Não foi possível cadastrar."})
+        res.status(400).json({msg :"Não foi possível cadastrar."});
     }
-})
+});
 
 app.put("/Treinamento/:id", VerificaId("Treinamento"), async (req, res) => {
-    const {tipo, quemrealiza, dragaoparticipante, tempo} = req.body;
+    const {tipo, quemrealiza, Dragaoparticipante, tempo} = req.body;
     id = req.params.id;
     try {
-
-            res.status(200).json({msg : " editado com sucesso!"})     
+        await db.query ("UPDATE Treinamento SET tipo=$1, quemrealiza=$2, Dragaoparticipante=$3, tempo=$4 WHERE id=$5", [tipo, quemrealiza, Dragaoparticipante, tempo, id])
+            res.status(200).json({msg : "Treinamento editado com sucesso!"});     
     } catch (error) {
-        res.status(404).json({msg : "Algo deu errado na edição."})        
+        res.status(404).json({msg : "Algo deu errado na edição."});        
     }
-})
+});
 
 app.delete("/Treinamento/:id", VerificaId("Treinamento"), async (req, res) => {
     id = req.params.id;
     try {
-
-            res.status(200).json({msg : "Deletado com sucesso!"}) 
+        await db.query("DELETE FROM Treinamento WHERE id=$1", [id])
+            res.status(200).json({msg : "Deletado com sucesso!"}); 
     } catch (error) {
-        res.status(400).json({msg : "Não encontrado!"})        
+        res.status(400).json({msg : "Não encontrado!"});        
     }
-})
+});
 
 
 app.listen(3000, () => {
